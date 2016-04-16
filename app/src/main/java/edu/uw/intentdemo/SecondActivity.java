9141435fda0e12a099b6a33eae3903a5a6743952
Joel Ross <joelross@uw.edu>
2016-04-16 07:58:01 -0700

    Add lecture starter code

---
+++ app/src/main/java/edu/uw/intentdemo/SecondActivity.java
@@ -0,0 +1,43 @@
+package edu.uw.intentdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.AdapterView;
+import android.widget.SimpleCursorAdapter;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "**DEMO 2**";
+
+    //for listview
+    private SimpleCursorAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        //action bar "back"
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
+        //TODO: fill in the link between columns and ids!
+        String[] cols = new String[]{};
+        int[] ids = new int[]{};
+
+        //should use a loader instead...
+        adapter = new SimpleCursorAdapter(
+                this,
+                R.layout.list_item,
+                null,
+                cols, ids,
+                0 //no flag
+        );
+        //set the adapter
+        AdapterView listView = (AdapterView)findViewById(R.id.listSecond);
+        listView.setAdapter(adapter);
+    }
+}
